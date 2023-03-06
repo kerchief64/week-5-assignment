c99d1d566d1a9964b0c405d361ee1cb54e070466
kerchief64 <kerchief64@protonmail.com>
2023-03-06 14:29:07 -0300

    first componentization

---
+++ src/routes/layout/dashboard/Mobile/MenuToggle.svelte
@@ -0,0 +1,20 @@
+<button
+	type="button"
+	class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden"
+>
+	<span class="sr-only">Open sidebar</span>
+	<svg
+		class="h-6 w-6"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		aria-hidden="true"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+		/>
+	</svg>
+</button>
